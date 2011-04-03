8e28306cc1227ca69e915bbee0245c050b423772
dave <dave@Laptop-of-HouQP.(none)>
2011-04-03 17:23:08 +0000

    some modifications which I cannot remember

--- main.asm
+++ main.asm
@@ -1,10 +1,11 @@
 ; test.asm 
 ;%include "io.mac"
 ;%include "asm_io.inc"
-%include "myio.inc"
+%include "myio.asm"
 
 segment .date
-	msg1 dd "Please input the first number :",0
+	msg1 db "@-----set the zero flag-----",10,0
+	msg1_len equ $-msg1
 	nextline db 0x0d,0x0a,0
 
 segment .bss
@@ -12,23 +13,21 @@ segment .bss
 	re resd 1
 
 segment .text
-	global main
+	global _start
+	;global main
 
-main:
+_start:
+;main:
+
 ; Input the first number
 		mov EAX,msg1
-		call print_string
-		call read_int 
-			mov [a],EAX
-; Subtract three numbers
-		mov AX,[a]
-		mov [re],AX
 		
 ; go to next line
-		mov EAX,nextline
-		call print_string
+	;	mov EAX,nextline
+	;	call print_string
+	my_print nextline,3
 		
-		dump_regs 1
+	;	dump_regs 1
 
 ;exit progrem
-myexit
+my_exit
